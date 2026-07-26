df9b7efc5f46390ae2a2f405e65e386dc0f97deb
BartLIma <barmarlima@gmail.com>
2026-07-26 01:08:39 -0300

    Update password and enhance access messages

--- Menu_1app.py
+++ Menu_1app.py
@@ -1,23 +1,29 @@
 import streamlit as st
 
 # Senha fixa
-senha_correta = "ditre123"
+senha_correta = "dit"
 senha = st.text_input("Digite a senha para acessar:", type="password")
 
 if senha == senha_correta:
-    st.success("Acesso liberado ✅")
+    # Mensagem de acesso liberado mais discreta
+    st.markdown(
+        "<p style='color:green; font-size:14px; font-weight:bold;'>Acesso liberado ✅</p>",
+        unsafe_allow_html=True
+    )
+
     st.title("📂 Menu Principal - Programas")
 
     st.write("Selecione abaixo o programa que deseja abrir:")
 
-    st.markdown("[🔍 ConsEquip](https://consequip-yg7w8sxbuqujhvu7spyntb.streamlit.app/)")
-    st.markdown("[📊 Conrepass](https://conrepass-frhucxskkdgmt2hxuq4kju.streamlit.app/)")
-    st.markdown("[📑 Semuspb](https://semuspb-z2u4ydkkdznpwuchwyau6f.streamlit.app/)")
+    # Links com descrições personalizadas
+    st.markdown("[🔍 Consultar Equipamentos](https://consequip-yg7w8sxbuqujhvu7spyntb.streamlit.app/)")
+    st.markdown("[📊 Consultar Transferências (convênios ou contratos de repasse)](https://conrepass-frhucxskkdgmt2hxuq4kju.streamlit.app/)")
+    st.markdown("[📑 Consultar Cadastro de Secretarias de Saúde](https://semuspb-z2u4ydkkdznpwuchwyau6f.streamlit.app/)")
 else:
     st.warning("Digite a senha correta para acessar o sistema.")
 
 # Rodapé discreto
 st.markdown(
-    "<p style='text-align:right; font-size:12px; color:green;'>Bartolomeu Lima</p>",
+    "<p style='text-align:right; font-size:12px; color:green;'>Bartolomeu Lima - Corecon-ES 1541</p>",
     unsafe_allow_html=True
 )
