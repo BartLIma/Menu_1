5f4fdb0525da197d482d997032d9a11a1ad06b48
BartLIma <barmarlima@gmail.com>
2026-07-26 18:37:48 -0300

    Fix button text for medical equipment link

--- Menu_1app.py
+++ Menu_1app.py
@@ -30,7 +30,7 @@ st.markdown(
 
 # Botões coloridos abrindo em nova guia
 st.markdown(
-    "<a class='menu-button green' href='https://consequip-yg7w8sxbuqujhvu7spyntb.streamlit.app/' target='_blank'>🔍 Equipamentos  Médico-Hospitalares - (Lista Renem)</a>",
+    "<a class='menu-button green' href='https://consequip-yg7w8sxbuqujhvu7spyntb.streamlit.app/' target='_blank'>🔍 Equipamentos  Médico-Hospitalares  (Lista Renem)</a>",
     unsafe_allow_html=True
 )
 st.markdown(
